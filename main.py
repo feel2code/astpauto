82e3a66e324c82447bab5c7bf31d31cd146a3214
minifrukt <89178705098@mail.ru>
2021-10-05 11:00:08 +0300

    reformed code, added comments, ignored logfile and user/pass, SQL scripts included to config hided files, choosing WO changed to GET request. Added English comments.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Приложение разработано для внутреннего оперативного создания тасок в интерфейсе таскменеджера IBM WebSphere
+# App coded for inside use in taskmanager IBM WebSphere to make work orders easier
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
@@ -11,118 +12,77 @@ from tkinter import scrolledtext
 import time
 from connectconf import *
 from scripts import *
-# запуск браузера в фоне
+
 opts = Options()
-opts.headless = True # раскомментировать для запуска в фоне
-# вход в астп
+opts.headless = True  # open browser in background
+# enter to taskmanager
 driver = webdriver.Firefox(executable_path='/Users/felixmac/Documents/PyCharmProjects/astpauto/geckodriver',
                            service_log_path='/Users/felixmac/Documents/PyCharmProjects/astpauto/driver.log',
                            options=opts)
-# домен скрыт в целях безопасности
+# domen hided cause of confidential
 driver.get('http://astp/maximo/')
-# логин пароль для входа в интерфейс IBM WebSphere
+# login and password of IBM WebSphere taskmanager hided in connectconf
 s_username = driver.find_element_by_name("username")
 s_password = driver.find_element_by_name('password')
 s_username.send_keys(username)
 s_password.send_keys(password)
-status = False
-while not status:
+element_dispayed_status = False
+while not element_dispayed_status:
     try:
-        status = driver.find_element_by_class_name('tiv_btn').is_displayed()
+        element_dispayed_status = driver.find_element_by_class_name('tiv_btn').is_displayed()
     except ImportError:
-        status = False
+        element_dispayed_status = False
 driver.find_element_by_class_name('tiv_btn').click()
-# старый способ перехода к РЗ
-'''
-status = False
-while not status:
-    try:
-        status = driver.find_element_by_id('m7f8f3e49_ns_menu_WO_MODULE_a').is_displayed()
-    except ImportError:
-        status = False
-driver.find_element_by_id('m7f8f3e49_ns_menu_WO_MODULE_a').click()
-status = False
-while not status:
-    try:
-        status = driver.find_element_by_id('m7f8f3e49_ns_menu_WO_MODULE_sub_changeapp_WOTRACK').is_displayed()
-    except ImportError:
-        status = False
-if status:
-    driver.find_element_by_id('m7f8f3e49_ns_menu_WO_MODULE_sub_changeapp_WOTRACK').click()
-'''
-def gecko():
-    rz = txt.get()
-    driver.get('http://astp/maximo/ui/login?event=loadapp&value=wotrack&additionalevent=useqbe&additionaleventvalue=wonum='+rz+'&forcereload=true')
-    status1 = False
-    while not status1:
+
+
+def enter_to_work_order():
+    work_order_number = work_order_entry.get()
+    path_to_work_order = 'http://astp/maximo/ui/login?event=loadapp&value=wotrack&additionalevent'
+    path_to_work_order += '=useqbe&additionaleventvalue=wonum='+work_order_number+'&forcereload=true'
+    driver.get(path_to_work_order)
+    element_dispayed_status1 = False
+    while not element_dispayed_status1:
         try:
-            status1 = driver.find_element_by_id('m3b854f9f-sc_div').is_displayed()
+            element_dispayed_status1 = driver.find_element_by_id('m3b854f9f-sc_div').is_displayed()
         except ImportError:
-            status1 = False
-    # создаем ДРЗ
+            element_dispayed_status1 = False
+    # making work order
     time.sleep(1)
     driver.find_element_by_id('m3b854f9f-sc_div').click()
-    status2 = False
-    while not status2:
+    element_dispayed_status2 = False
+    while not element_dispayed_status2:
         try:
-            status2 = driver.find_element_by_id('ma7efa7a3-tb').is_displayed()
+            element_dispayed_status2 = driver.find_element_by_id('ma7efa7a3-tb').is_displayed()
         except ImportError:
-            status2 = False
-    # Вводим данные по задаче
+            element_dispayed_status2 = False
+    # click to entry of js window
     time.sleep(1)
     driver.find_element_by_id('ma7efa7a3-tb').click()
+# closing work order after making changes
 
-# старый метод выбора таски
-def spin():
-    rz = txt.get()
-    driver.find_element_by_id('quicksearch').click()
-    driver.find_element_by_id('quicksearch').send_keys(rz)
-    driver.find_element_by_id('quicksearch').click()
-    driver.find_element_by_id('quicksearch').send_keys(Keys.RETURN)
-    status1 = False
-    while not status1:
+
+def closing_work_order():
+    element_dispayed_status3 = False
+    while not element_dispayed_status3:
         try:
-            status1 = driver.find_element_by_id('m3b854f9f-sc_div').is_displayed()
+            element_dispayed_status3 = driver.find_element_by_id('m15f1c9f0-pb').is_displayed()
         except ImportError:
-            status1 = False
-    # создаем ДРЗ
-    time.sleep(1)
-    driver.find_element_by_id('m3b854f9f-sc_div').click()
-    status2 = False
-    while not status2:
-        try:
-            status2 = driver.find_element_by_id('ma7efa7a3-tb').is_displayed()
-        except ImportError:
-            status2 = False
-    # Вводим данные по задаче
-    time.sleep(1)
-    driver.find_element_by_id('ma7efa7a3-tb').click()
-
-# закрыть после создания ДРЗ
-
-
-def zakrit():
-    status3 = False
-    while not status3:
-        try:
-            status3 = driver.find_element_by_id('m15f1c9f0-pb').is_displayed()
-        except ImportError:
-            status3 = False
+            element_dispayed_status3 = False
     time.sleep(2)
     driver.find_element_by_id('m15f1c9f0-pb').click()
-# кнопка для скриптов
+# complex selects from DB
 
 
-def clicked():
-    snils = txt2.get('1.0', '1.11')
-    # импорт SQL скриптов для выполнения. Содержимое скрыто в целях конфиденциальности
+def scripts_select_button():
+    snils = scripts_entry.get('1.0', '1.11')
+    # imported SQL scripts hided in scripts.py cause of confidential
     script = a1 + " " + a2 + " " + a3 + " " + a4 + " " + a5 + " " + a6 + " " + a7 + " " + a8 + " " + a9 + " " + a10
     script = script + " " + a11 + " " + a12 + " " + a13 + " " + a14 + " " + a15 + " " + a16 + " " + a17 + " " + a18
     script = script + " " + a19 + " " + a20 + " " + a21 + " " + a22 + " " + a23 + " " + a24 + " " + a25 + " " + a26
     script = script + " " + a27 + " " + a28
     script = script.replace('123456789', snils)
     pyperclip.copy(script)
-    gecko()
+    enter_to_work_order()
     driver.find_element_by_id('ma7efa7a3-tb').send_keys('Выполнить селект')
     driver.find_element_by_id('m6bda82c1-ta').click()
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "a")
@@ -131,19 +91,19 @@ def clicked():
     driver.find_element_by_id('m9e96a86b-tb').click()
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.2. \ 25.2.2.')
     driver.find_element_by_id('m1317c3f5-pb').click()
-    zakrit()
-    txt2.delete('1.0', tk.END)
+    closing_work_order()
+    scripts_entry.delete('1.0', tk.END)
     i = 0
     while i < 35:
-        txt.delete(0)
+        work_order_entry.delete(0)
         i += 1
-# кнопка для выборки
+# simple select from DB
 
 
-def clickedvib():
-    script = txt2.get('1.0', tk.END)
+def select_button():
+    script = scripts_entry.get('1.0', tk.END)
     pyperclip.copy(script)
-    gecko()
+    enter_to_work_order()
     driver.find_element_by_id('ma7efa7a3-tb').send_keys('Приложить выборку')
     driver.find_element_by_id('m6bda82c1-ta').click()
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "a")
@@ -152,19 +112,19 @@ def clickedvib():
     driver.find_element_by_id('m9e96a86b-tb').click()
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.2. \ 25.2.2.')
     driver.find_element_by_id('m1317c3f5-pb').click()
-    zakrit()
-    txt2.delete('1.0', tk.END)
+    closing_work_order()
+    scripts_entry.delete('1.0', tk.END)
     i = 0
     while i < 35:
-        txt.delete(0)
+        work_order_entry.delete(0)
         i += 1
-# кнопка ручного селекта
+# button for manual select from DB
 
 
-def clickedmanual():
-    script = txt2.get('1.0', tk.END)
+def manual_select_button():
+    script = scripts_entry.get('1.0', tk.END)
     pyperclip.copy(script)
-    gecko()
+    enter_to_work_order()
     driver.find_element_by_id('ma7efa7a3-tb').send_keys('Выполнить селект')
     driver.find_element_by_id('m6bda82c1-ta').click()
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "a")
@@ -173,19 +133,19 @@ def clickedmanual():
     driver.find_element_by_id('m9e96a86b-tb').click()
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.2. \ 25.2.2.')
     driver.find_element_by_id('m1317c3f5-pb').click()
-    zakrit()
-    txt2.delete('1.0', tk.END)
+    closing_work_order()
+    scripts_entry.delete('1.0', tk.END)
     i = 0
     while i < 35:
-        txt.delete(0)
+        work_order_entry.delete(0)
         i += 1
-# кнопка апдейта
+# button for update scripts in DB
 
 
-def clickedupdate():
-    script = txt2.get('1.0', tk.END)
+def update_button():
+    script = scripts_entry.get('1.0', tk.END)
     pyperclip.copy(script)
-    gecko()
+    enter_to_work_order()
     driver.find_element_by_id('ma7efa7a3-tb').send_keys('Просьба согласовать выполнение скриптов')
     driver.find_element_by_id('m6bda82c1-ta').click()
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "a")
@@ -194,61 +154,32 @@ def clickedupdate():
     driver.find_element_by_id('m9e96a86b-tb').click()
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.7.')
     driver.find_element_by_id('m1317c3f5-pb').click()
-    zakrit()
-    txt2.delete('1.0', tk.END)
+    closing_work_order()
+    scripts_entry.delete('1.0', tk.END)
     i = 0
     while i < 35:
-        txt.delete(0)
+        work_order_entry.delete(0)
         i += 1
-# проверка выполнения, не реализовано
-
-
-def check():
-    rz = txt.get()
-    driver.find_element_by_id('quicksearch').click()
-    driver.find_element_by_id('quicksearch').send_keys(rz)
-    driver.find_element_by_id('quicksearch').click()
-    driver.find_element_by_id('quicksearch').send_keys(Keys.RETURN)
-    status4 = False
-    while not status4:
-        try:
-            status4 = driver.find_element_by_id('m4326cf1d-tab_anchor').is_displayed()
-        except ImportError:
-            status4 = False
-    time.sleep(2)
-    driver.find_element_by_id('m4326cf1d-tab_anchor').click()
-    status5 = False
-    while not status5:
-        try:
-            status5 = driver.find_element_by_id('toolactions_RESETREC-tbb_anchor').is_displayed()
-        except ImportError:
-            status5 = False
-    time.sleep(2)
-    driver.find_element_by_id('toolactions_RESETREC-tbb_anchor').click()
-# настройка окна
+# window UI parameters
 
 
 window = Tk()
-top = Frame(window)
-bottom = Frame(window)
-bottom2 = Frame(window)
-window.title('Выгрузка данных по СНИЛСУ в АСТП')
+top_part_of_frame = Frame(window)
+bottom_part_of_frame = Frame(window)
+window.title('Выгрузка данных по СНИЛС в АСТП')
 window.geometry('550x400')
-txt = Entry(top, width=19)
-txt2 = scrolledtext.ScrolledText(bottom, width=70, height=20)
-btn1 = Button(top, text="Выборка", command=clickedvib)
-btn = Button(top, text="Скрипты", command=clicked)
-btn2 = Button(top, text="Ручной селект", command=clickedmanual)
-btn3 = Button(top, text="Ручной апдейт", command=clickedupdate)
-# btnchck = Button(bottom2, text='Проверка', command=check)
-top.pack()
-bottom.pack()
-bottom2.pack()
-txt.pack(side=LEFT)
-btn1.pack(side=LEFT)
-btn.pack(side=LEFT)
-btn2.pack(side=LEFT)
-btn3.pack(side=LEFT)
-txt2.pack(side=LEFT)
-# btnchck.pack(side=LEFT)
+work_order_entry = Entry(top_part_of_frame, width=19)
+scripts_entry = scrolledtext.ScrolledText(bottom_part_of_frame, width=70, height=20)
+button_for_select = Button(top_part_of_frame, text="Выборка", command=select_button)
+button_for_scripts = Button(top_part_of_frame, text="Скрипты", command=scripts_select_button)
+button_for_manual_select = Button(top_part_of_frame, text="Ручной селект", command=manual_select_button)
+button_for_update = Button(top_part_of_frame, text="Ручной апдейт", command=update_button)
+top_part_of_frame.pack()
+bottom_part_of_frame.pack()
+work_order_entry.pack(side=LEFT)
+button_for_select.pack(side=LEFT)
+button_for_scripts.pack(side=LEFT)
+button_for_manual_select.pack(side=LEFT)
+button_for_update.pack(side=LEFT)
+scripts_entry.pack(side=LEFT)
 window.mainloop()
