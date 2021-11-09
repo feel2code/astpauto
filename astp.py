7c2e15c9a6d2d32a30fe860008af39f83919a031
minifrukt <89178705098@mail.ru>
2021-11-09 12:45:59 +0300

    reformed code, added comments, ignored logfile and user/pass, SQL scripts included to config hided files, choosing WO changed to GET request. Changed path to relative. Added English comments.

--- astp.py
+++ astp.py
@@ -109,7 +109,7 @@ def scripts_select_button():
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "v")
     click_button('m9e96a86b-tb')
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.2. \ 25.2.2.')
-    time.sleep(2)
+    time.sleep(3)
     click_button('m1317c3f5-pb')
     closing_work_order()
 
@@ -128,7 +128,7 @@ def select_button():
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "v")
     click_button('m9e96a86b-tb')
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.2. \ 25.2.2.')
-    time.sleep(2)
+    time.sleep(3)
     click_button('m1317c3f5-pb')
     closing_work_order()
 
@@ -147,7 +147,7 @@ def manual_select_button():
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "v")
     click_button('m9e96a86b-tb')
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.2. \ 25.2.2.')
-    time.sleep(2)
+    time.sleep(3)
     click_button('m1317c3f5-pb')
     closing_work_order()
 
@@ -166,7 +166,7 @@ def update_button():
     driver.find_element_by_id('m6bda82c1-ta').send_keys(Keys.COMMAND + "v")
     click_button('m9e96a86b-tb')
     driver.find_element_by_id('m9e96a86b-tb').send_keys(r'25. \ 25.7.')
-    time.sleep(2)
+    time.sleep(3)
     click_button('m1317c3f5-pb')
     closing_work_order()
 
